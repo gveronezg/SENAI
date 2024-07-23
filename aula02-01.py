9d2c6703afaf49d68db986a7941cf6a2ee85c49e
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-07-23 19:59:12 -0300

    Create aula02-01.py

---
+++ aula02-01.py
@@ -0,0 +1,9 @@
+a = 5
+b = 3
+
+print(f'A = B : {a == b}')
+print(f'A <> B : {a != b}')
+print(f'A > B : {a > b}')
+print(f'A < B : {a < b}')
+print(f'A >= B : {a >= b}')
+print(f'A <= B : {a <= b}')
