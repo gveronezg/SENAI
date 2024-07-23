6aa102058ecbf5a8c0391f877807191487f9738a
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-07-23 20:02:17 -0300

    Update aula02-01.py

--- aula02-01.py
+++ aula02-01.py
@@ -1,9 +1,9 @@
 a = 5
 b = 3
 
-print(f'A = B : {a == b}')
-print(f'A <> B : {a != b}')
-print(f'A > B : {a > b}')
-print(f'A < B : {a < b}')
-print(f'A >= B : {a >= b}')
-print(f'A <= B : {a <= b}')
+print(f'A = B : {a == b}') # False
+print(f'A <> B : {a != b}') # True
+print(f'A > B : {a > b}') # True
+print(f'A < B : {a < b}') # False
+print(f'A >= B : {a >= b}') # True
+print(f'A <= B : {a <= b}') # False
