d139610bb1d331ae243df1667f3441577cecacbe
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-07-23 20:12:17 -0300

    Create aula02-02.py

---
+++ aula02-02.py
@@ -0,0 +1,10 @@
+a = 5
+b = 8
+c = 1
+
+print(f'A = B AND B > C : {a == b and b > c}')
+print(f'A <> B OR B < C : {a != b or b < c}')
+print(f'NOT A > B : {not a > b}')
+print(f'A < B AND B > C : {a < b and b > c}')
+print(f'A >= B OR B = C : {a >= b or b == c}')
+print(f'NOT A <= B : {not a <= b}')
