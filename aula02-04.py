eef6dab81c4ab6bb2c2066e13443a41c391fe387
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-07-23 21:50:32 -0300

    Create aula02-04.py

---
+++ aula02-04.py
@@ -0,0 +1,6 @@
+# 1. Solicite ao usuário que digite seu nome e imprima uma saudação com esse nome. (Fulano, Seja Bem Vindo)
+
+nome = input('Entre com seu nome: ')
+print('{}, Seja Bem Vindo!'.format(nome))
+
+# 2. Peça ao usuário para digitar um número e imprima o dobro desse número.
