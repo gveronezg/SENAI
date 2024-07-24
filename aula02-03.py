bed9da2f9ba9cac24d1a5b8baab81d4d230389ae
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-07-23 21:49:58 -0300

    Create aula02-03.py

---
+++ aula02-03.py
@@ -0,0 +1,34 @@
+"""
+Exercícios Lógica:
+1. Verifique se 2 é maior que 1
+2. Teste se 5 é menor que 10.
+3. Verifique se 'a' é igual a 'a'.
+4. Teste se 3 é igual a 3 ou 4.
+5. Verifique se 8 é maior que 5 e menor que 10.
+6. Teste se 0 é igual a 0 ou diferente de 1.
+7. Verifique se 9 é menor que 10 ou maior que 20.
+8. Teste se 'x' não é igual a 'y'.
+9. Verifique se 1 é menor que 2 e 3 é maior que 2.
+10. Teste se 'gato' é igual a 'gato' ou 'cachorro'.
+11. Teste se 10 é maior que 5 ou menor que 15.
+12. Verifique se 2 mais 2 é igual a 4.
+13. Teste se 7 é menor que 8 e maior que 6.
+14. Teste se 15 é maior que 10 ou menor que 5.
+15. Verifique se 100 é igual a 100 ou 50.
+"""
+
+print(f'1.Verifique se 2 é maior que 1: {2>1}')
+print(f'2.Teste se 5 é menor que 10: {5<10}')
+print(f"3.Verifique se 'a' é igual a 'a': {'a'=='a'}")
+print(f'4.Teste se 3 é igual a 3 ou 4: {3==(3 or 4)}')
+print(f'5.Verifique se 8 é maior que 5 e menor que 10: {5<8<10}')
+print(f'6.Teste se 0 é igual a 0 ou diferente de 1: {0==0 or 0!=1}')
+print(f'7.Verifique se 9 é menor que 10 ou maior que 20: {10>9 or 9>20}')
+print(f'8.Teste se "x" não é igual a "y": {not"x"=="y"}')
+print(f'9.Verifique se 1 é menor que 2 e 3 é maior que 2: {1<2 and 3>2}')
+print(f'10.Teste se "gato" é igual a "gato" ou "cachorro": {"gato"==("gato"or"cachorro")}')
+print(f'11.Teste se 10 é maior que 5 ou menor que 15: {10>5 or 15>10}')
+print(f'12.Verifique se 2 mais 2 é igual a 4: {2+2 == 4}')
+print(f'13.Teste se 7 é menor que 8 e maior que 6: {7<8 and 7>6}')
+print(f'14.Teste se 15 é maior que 10 ou menor que 5: {15>10 or 15<5}')
+print(f'15.Verifique se 100 é igual a 100 ou 50: {100 == 100 or 100 == 50}')
