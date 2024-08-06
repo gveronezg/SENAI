b4fb271c68ca60ac347bfc10d6c215ec949d59ab
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-08-06 19:45:31 -0300

    Create lista1.py

---
+++ lista1.py
@@ -0,0 +1,58 @@
+# Lista 1.1) Imprima "Olá, mundo!".
+print('Hello World!')
+
+# Lista 1.2) Imprima o número 123.
+print("123")
+
+# Lista 1.3) Imprima uma frase que inclui um número, como "O número é: 10".
+print("O número é: 10")
+
+# Lista 1.4) Imprima "Olá!".
+print("Ola!")
+
+# Lista 1.5) Imprima o número 5.
+print(5)
+
+# Lista 1.6) Imprima "Meu nome é João".
+print("Meu nome é João")
+
+# Lista 1.7) Imprima "Python é legal!".
+print("Python é legal!")
+
+# Lista 1.8) Imprima "1, 2, 3" em uma única linha.
+print("1, 2, 3")
+
+# Lista 1.9) Imprima "Hoje é um bom dia!" usando aspas.
+print('"Hoje é um bom dia!"')
+
+# Lista 1.10)Imprima "Aprendendo Python..." em três linhas diferentes.
+print("Aprendendo Python...")
+print("Aprendendo Python...")
+print("Aprendendo Python...")
+
+# Lista 1.11)Imprima "A soma de 2 e 3 é:" e depois o resultado da soma.
+soma = (2+3)
+print(f"A soma de 2 e 3 é: {soma}")
+
+# Lista 1.12)Imprima "Gostei disso!" e uma exclamação no final.
+print("Gostei disso!")
+
+# Lista 1.13)Imprima a palavra "Cachorro" em letras maiúsculas.
+print("CACHORRO")
+
+# Lista 1.14)Imprima "Estou aprendendo" seguido de "Python!".
+print("Estou aprendendo " + "Python!")
+
+# Lista 1.15)Imprima "Feliz", "Aniversário" separadas por um espaço.
+print("Feliz" + " " + "Aniversário")
+
+# Lista 1.16)Imprima "Bom" e "trabalho!" na mesma linha.
+print("Bom " + "trabalho!")
+
+# Lista 1.17)Imprima um número negativo, como "-10".
+negativo = -10
+print(negativo)
+
+# Lista 1.18)Imprima uma frase que inclua uma data, como "Hoje é 16 de julho".
+data = "06 de agosto"
+print("Hoje é "+data)
