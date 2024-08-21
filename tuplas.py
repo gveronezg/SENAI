816d784dde3b10d0e9bca0378f5c14b422a5cf09
Gabriel Veronez Giolo <90968620+gveronezg@users.noreply.github.com>
2024-08-21 19:16:59 -0300

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -1,34 +1,5 @@
-"""
-indice = (0,1,2)
-lanche = ('Tomate','Pão','Alface')
-posição = (-3,-2,-1)
-print(lanche)
-print(lanche[1])
-print(lanche[-1])
-print(lanche[2])
-print(lanche[-3])
-print(lanche[:2])
-print(lanche[1:])
-
-for i in lanche:
-    print(f'Eu comi {i}')
-    
-print(len(lanche))
-
-for x in range(0,len(lanche)):
-    print(f'Eu comi {x}')
-    
-# coloca em ordem alfabética
-print(sorted(lanche))
-
-pessoa = ('Gabs', 28, 'H', 70)
-print(pessoa)
-
-del(pessoa)
-"""
-
 # Lista 12.1. Crie uma tupla vazia.
-tupla = ()
+tupla = ('')
 
 # Lista 12.2. Crie uma tupla com 5 números inteiros.
 numeros = (1,2,3,4,5)
